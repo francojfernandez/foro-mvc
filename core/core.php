4de732357fec9ff0aa9967e409d784fa60345321
francojfernandez <francofernandez_0412@hotmail.com>
2017-01-06 12:29:36 -0300

    Updates

--- core/core.php
+++ core/core.php
@@ -4,5 +4,7 @@
 #Constantes en mayusculas
 define('HTML_DIR','html/');
 define('APP_TITLE','FORO MVC');
-
+define('APP_COPY', 'Copyright &copy; '. date('Y',time(). ' Decode'));
+define('APP_URL', 'http://localhost/github/foro-mvc/');
+require('vendor/autoload.php');
 ?>
