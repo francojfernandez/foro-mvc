2f0088e948630d68671986f03688b740a1a70fd1
Franco <francofernandez_0412@hotmail.com>
2017-01-25 05:57:34 -0300

    Updates

--- core/bin/ajax/goReg.php
+++ core/bin/ajax/goReg.php
@@ -1,3 +1,22 @@
 <?php
-echo 'funciona';
+
+$db = new Conexion();
+
+$pass = Encrypt($_POST['pass']);
+$user = $db->real_escape_string ($_POST['user']);
+$email = $db->real_escape_string ($_POST['email']);
+
+$sql = $db->query("SELECT user from users where user ='$user' or email='$email' LIMIT 1");
+if($db->rows($sql)==0){
+
+}else{
+	$usuario = $db->recorrer($sql)[0];
+	if(strtolower($user)== strtolower($usuario)){
+		
+	}else{
+		
+	}
+}
+$db->close();
+
 ?>
